c4b90678916922d8a14209dd240a3076cd88e747
Matthew Small <35241491+m1small@users.noreply.github.com>
2018-11-30 13:43:11 -0800

    New build.sh

---
+++ stacksmith/user-scripts/build.sh
@@ -0,0 +1,15 @@
+#remove old docker
+sudo yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-selinux \
+                  docker-engine-selinux \
+                  docker-engine
+                  
+#install new docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
